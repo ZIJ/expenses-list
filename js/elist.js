a5cf4d820ad271ad95ffe27c7f2cfa397c12ac1e
Igor Zalutski <igor.zalutsky@gmail.com>
2012-08-10 04:26:09 +0300

    Added Wrapper with remove and append functionality

--- js/elist.js
+++ js/elist.js
@@ -17,7 +17,7 @@
         if (!condition) {
             throw new Error(errorMessage ? errorMessage : "Assertion failed");
         }
-    }
+    };
 
     elist.assert(this.document !== undefined, "Document undefined");   // checking document's presence
     var doc = this.document;
@@ -30,6 +30,65 @@
         //TODO: crossbrowser "ready"
         doc.addEventListener("DOMContentLoaded", listener, false);
         return elist;
+    };
+    /**
+     * Selects DOM element by it's ID
+     * @param id
+     * @return
+     */
+    elist.byId = function(id){
+        var node = document.getElementById(id);
+        return new Wrapper(node);
+    };
+    /**
+     * Wraps an element into a Wrapper :)
+     * @param element
+     * @return {Wrapper}
+     */
+    elist.wrap = function(element) {
+        return new Wrapper(element);
     }
+    /**
+     * Wraps DOM node adding some useful features
+     * @param element
+     * @constructor
+     */
+    function Wrapper(element) {
+        this.element = element ? element : null;
+    }
+
+    /**
+     * Checks whether the wrapper contains an element or not
+     * @return {Boolean}
+     */
+    Wrapper.prototype.isEmpty = function(){
+        return !this.element;
+    };
+    /**
+     * Removes node from dom
+     * @return Removed node, wrapped
+     */
+    Wrapper.prototype.remove = function(){
+        if (!this.isEmpty()) {
+            var node = this.element;
+            node.parentNode.removeChild(node);
+        }
+        return this;
+    };
+    /**
+     * Inserts child node to the end of children list
+     * @param content
+     * @return {*}
+     */
+    Wrapper.prototype.append = function(content){
+
+        if (!this.isEmpty()) {
+            var toInsert = content instanceof Wrapper ? content.element :
+                           content instanceof HTMLElement ? content :
+                           doc.createTextNode(content);
+            this.element.appendChild(toInsert);
+        }
+        return this;
+    };
 
 })();
--- js/tests.js
+++ js/tests.js
@@ -6,10 +6,10 @@
  */
 
 (function(){
-    var tests = {};             //array of test functions
-    var toRun = ["ready"];      //names of tests to run
-    tests.ready = function() {
-        console.log("testing ready...");
+    var tests = {};                 //test functions
+    var toRun = ["append"];         //names of tests to run
+
+    tests.ready = function() {                  //ready should fire before onload
         elist.ready(function(){
             console.log("ready fired");
         });
@@ -17,10 +17,40 @@
             console.log("onload fired");
         };
     };
-    //running tests
+
+    tests.remove = function() {                 //adds element to dom, searches for it by id, removes, searches again
+        elist.ready(function(){
+            var elem = document.createElement("div");
+            elem.id = 13;
+            document.body.appendChild(elem);
+            var found = elist.byId(13);
+            console.log("found? " + !found.isEmpty());
+            found.remove();
+            var notfound = elist.byId(13);
+            console.log("removed? " + notfound.isEmpty());
+        });
+    };
+
+    tests.append = function() {                 // appends an element, wrapped element and string
+        elist.ready(function() {
+            var div1 = document.createElement('div');
+            div1.innerHTML = "div1";
+            var div2 = document.createElement('div');
+            div2.innerHTML = "div2";
+            elist.wrap(document.body)
+                .append(div1)
+                .append(elist.wrap(div2))
+                .append("TEXT NODE");
+        });
+    };
+    /**
+     * Running tests
+     */
     (function run(){
         for(var i = 0; i < toRun.length; i++) {
-            tests[toRun[i]]();
+            var testName = toRun[i];
+            console.log("running " + testName + " test...");
+            tests[testName]();
         }
     })();
 })();
