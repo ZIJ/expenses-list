7bec95602fd542f2f62c85faf7cd108d2f887293
Igor Zalutski <igor.zalutsky@gmail.com>
2012-08-13 06:58:12 +0300

    Project structure change, also added and tested AmountView
    Added /src/core directory, updated grunt.js for new build process

---
+++ src/views/amountView.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Igor Zalutsky on 13.08.12 at 6:37
+ */
+
+(function () {
+    "use strict";
+    // publishing namespace
+    if (!window.elist) {
+        window.elist = {};
+    }
+    var elist = window.elist;
+
+
+}());
